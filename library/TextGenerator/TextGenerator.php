f6660cd70de450719c90628fb9b4fc397dfcba2d
Eugene Myazin <meniam@gmail.com>
2013-07-19 12:30:25 +0400

    fixed inner

--- library/TextGenerator/TextGenerator.php
+++ library/TextGenerator/TextGenerator.php
@@ -7,12 +7,12 @@ class TextGenerator
     public static function factory($template, array $options = array())
     {
         $template = trim($template);
-        if (mb_strpos($template, '{', null, 'utf8') === 0) {
-            $template = trim($template, '{}');
+        if (mb_strpos(trim($template), '{', null, 'utf8') === 0) {
+            $template = mb_substr($template, 1, -1, 'UTF-8');
             return new XorPart($template, $options);
         }
         if (mb_strpos($template, '[', null, 'utf8') === 0) {
-            $template = trim($template, '[] ');
+            $template = mb_substr($template, 1, -1, 'UTF-8');
             return new OrPart($template, $options);
         }
         //print_r($template . "\n");
