834671f8bf6a919ee100ad472591e32eae0f5ea0
Eugene Myazin <meniam@gmail.com>
2013-07-19 13:47:58 +0400

    replace

--- library/TextGenerator/TextGenerator.php
+++ library/TextGenerator/TextGenerator.php
@@ -4,11 +4,15 @@ namespace TextGenerator;
 
 class TextGenerator
 {
-    protected $replaceList = array();
+    protected static $replaceList;
 
     public static function factory($template, array $options = array())
     {
         $template = (string)$template;
+
+        if ($replaceList = self::getReplaceList()) {
+            $template = str_replace(array_keys($replaceList), array_values($replaceList), $template);
+        }
 
         if (preg_match_all('#(?:\[|\{)((?:(?:[^\[\{\]\}]+)|(?R))*)(?:\]|\})#', $template, $m) > 1) {
             return new Part($template, $options);
@@ -25,5 +29,26 @@ class TextGenerator
         }
 
         return new Part($template, $options);
+    }
+
+    public static function getReplaceList()
+    {
+        return self::$replaceList;
+    }
+
+    public static function addReplaceList($array)
+    {
+        if (is_array($array)) {
+            foreach ($array as $k => &$v) {
+                self::addReplace($k, $v);
+            }
+        }
+    }
+
+    public static function addReplace($name, $value)
+    {
+        $name = trim($name, '%');
+        $name = '%%' . $name . '%%';
+        self::$replaceList[$name] = (string)$value;
     }
 }
